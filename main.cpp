9eb93430a45e1b9f0d8e62c98221a1a3666d6824
H3cth0r <43997408+H3cth0r@users.noreply.github.com>
2021-12-02 17:40:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,44 +25,44 @@ using namespace std;
 // argv == argument vector(string of characters). literally arr of string, which can be indexed.
 int main(int argc, char* argv[]){
 
-    if(argc <= 1) exit(0);
-    if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0){help();exit(0);}
+    if(argc <= 1) exit(0);      // O(1)
+    if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0){help();exit(0);}   // O(1)
 
-    LinkedList<char*> commands = LinkedList<char*>();      // queue creation
-    string route="abc";int number_breached_directions = 5; // variables to store input
-    LinkedList<Server>  auxiliar_ll;
-    int auxiliar_ll_size;
-    for(int i = 1; i < argc; i++){
-        commands.queue(argv[i]);
-        if(strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--file") == 0){route = argv[i+1];i++;}
-        if(strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--directions") == 0){number_breached_directions = atoi(argv[i+1]);i++;}
+    LinkedList<char*> commands = LinkedList<char*>();      // queue creation  O(1)
+    string route="abc";int number_breached_directions = 5; // variables to store input  O(1)
+    LinkedList<Server>  auxiliar_ll;                        // O(1)
+    int auxiliar_ll_size;                                   // O(1)
+    for(int i = 1; i < argc; i++){                          // O(n)
+        commands.queue(argv[i]);                            // O(n)
+        if(strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--file") == 0){route = argv[i+1];i++;}        // O(1)
+        if(strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--directions") == 0){number_breached_directions = atoi(argv[i+1]);i++;}       // O(1)
     }
-    string command = commands.dequeue();
-    read_file(route, auxiliar_ll);
-    auxiliar_ll_size = auxiliar_ll.length();
-    Hashtable<string, int> attacked_ports(pow(auxiliar_ll_size,2.3));
-    Hashtable<string, int> directions(pow(auxiliar_ll_size,2.3));
-    Hashtable<string, int> sms_frequency(pow(auxiliar_ll_size,2.3));
-    Hashtable<int, int> weeks(pow(auxiliar_ll_size,2.3));
+    string command = commands.dequeue();                    // O(1)
+    read_file(route, auxiliar_ll);                          // O(n)
+    auxiliar_ll_size = auxiliar_ll.length();                // O(n)
+    Hashtable<string, int> attacked_ports(pow(auxiliar_ll_size,2.3));       // O(1)
+    Hashtable<string, int> directions(pow(auxiliar_ll_size,2.3));           // O(1)
+    Hashtable<string, int> sms_frequency(pow(auxiliar_ll_size,2.3));        // O(1)
+    Hashtable<int, int> weeks(pow(auxiliar_ll_size,2.3));                   // O(1)
 
 
-    while(!commands.is_empty()){
-        command = commands.dequeue();
-        if(command == "-a" || command == "--attacked"){
-            cout << "========= BREACHED PORTS ==========" << endl;
-            count_briched_ports(auxiliar_ll, attacked_ports, auxiliar_ll_size);
+    while(!commands.is_empty()){ // O(n)
+        command = commands.dequeue();   // O(1)
+        if(command == "-a" || command == "--attacked"){ // O(1)
+            cout << "========= BREACHED PORTS ==========" << endl;  // O(1)
+            count_briched_ports(auxiliar_ll, attacked_ports, auxiliar_ll_size); // O(n)
         }
-        if(command == "-d" ||command == "--directions"){
-            cout << "========= MOST BREACHED IPs =========" << endl;
-            most_breached_ips(auxiliar_ll, directions, auxiliar_ll_size, 5);
+        if(command == "-d" ||command == "--directions"){            // O(1)
+            cout << "========= MOST BREACHED IPs =========" << endl;    // O(1)
+            most_breached_ips(auxiliar_ll, directions, auxiliar_ll_size, 5);    // O(n^2)
         }
-        if(command == "--frequency"){
-            cout << "========= FREQUENCY OF EACH MESSAGE =======" << endl;
-            message_frequency(auxiliar_ll, sms_frequency, auxiliar_ll_size);
+        if(command == "--frequency"){   // O(1)
+            cout << "========= FREQUENCY OF EACH MESSAGE =======" << endl;  // O(1)
+            message_frequency(auxiliar_ll, sms_frequency, auxiliar_ll_size);    // O(n)
         }
-        if(command == "--dates"){
-            cout << "======== MOST VULNERABLE WEEK OF THE YEAR =====" << endl;
-            most_vulnerable_week(auxiliar_ll, weeks, auxiliar_ll_size);
+        if(command == "--dates"){           // O(1)
+            cout << "======== MOST VULNERABLE WEEK OF THE YEAR =====" << endl;  // O(1)
+            most_vulnerable_week(auxiliar_ll, weeks, auxiliar_ll_size);         // O(n)
         }
 
     }
